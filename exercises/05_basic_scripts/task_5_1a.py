4d9865efe72273cb31193a449e04b9a1e77a3177
beh0ld <last.dekabrist@gmail.com>
2019-03-05 19:33:40 +0300

    finished exercises update

--- exercises/05_basic_scripts/task_5_1a.py
+++ exercises/05_basic_scripts/task_5_1a.py
@@ -30,32 +30,38 @@ Mask:
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
-network = input('Enter network(n.n.n.n/n):')
-network = input()
-print(network)
-#network = '172.16.31.0/22'
+#network = input('Enter network(n.n.n.n/n):')
+#network = input()
+#print(network)
+network = '172.16.31.200/25'
 network = network.split('/')
 ip = str(network[0])
+print(ip)
 ip = ip.split('.')
+
 a = int(ip[0])
 b = int(ip[1])
 c = int(ip[2])
 d = int(ip[3])
 p = int(network[1])
 addr_bits = list(('1'*int(p)).zfill(32))
+print(addr_bits)
 addr_bits.reverse()
-e = int("".join(addr_bits[0:8]))
-f = int("".join(addr_bits[8:16]))
-g = int("".join(addr_bits[16:24]))
-i = int("".join(addr_bits[24:32]))
-u = int(("".join(addr_bits[0:8])),2)
-k = int(("".join(addr_bits[8:16])),2)
-l = int(("".join(addr_bits[16:24])),2)
-o = int(("".join(addr_bits[24:32])),2)
+
+a1 = int("".join(addr_bits[0:8]))
+b1 = int("".join(addr_bits[8:16]))
+c1 = int("".join(addr_bits[16:24]))
+d1 = int("".join(addr_bits[24:32]))
+
+a2 = int(("".join(addr_bits[0:8])),2)
+b2 = int(("".join(addr_bits[8:16])),2)
+c2 = int(("".join(addr_bits[16:24])),2)
+d2 = int(("".join(addr_bits[24:32])),2)
+
 network_template = ''' 
          Network:
          {0:<8} {1:<8} {2:<8} {3:<8}
-         {0:08b} {1:08b} {2:08b} {3:08b}
+         {0:08} {1:08} {2:08} {3:08}
          '''
 prefix_template = '''
          Mask:
@@ -63,7 +69,8 @@ prefix_template = '''
          {5:<8} {6:<8} {7:<8} {8:<8}
          {1:<08} {2:<08} {3:<08} {4:<08}
          '''
+
 print('\n' + '-' * 80)
 print(network_template.format(a, b, c, d))
-print(prefix_template.format(p, e, f , g, i, u, k, l, o))
+print(prefix_template.format(p, a1, b1 , c1, d1, a2, b2, c2, d2))
 print('\n' + '-' * 80)
